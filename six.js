44516f1ac78ff66d94d4de0b3816cc3377cc5205
lawal ridwan <lawalridwanullahi20@gmail.com>
2022-03-21 06:30:37 -0700

    Added js and html files

---
+++ six.js
@@ -0,0 +1,7 @@
+const items =[];
+console.log('i think the value of the array is my favorite animals');
+console.log(items);
+const animals =["dog", "tiger", "lion"]
+console.log(animals);
+animals[3]="cat";
+console.log(animals);
